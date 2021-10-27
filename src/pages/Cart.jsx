d26c6ce57b297068ff26be5906cf410d902a22d7
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2021-10-27 11:49:47 -0700

    add: product details and subtotal area

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,3 +1,4 @@
+import { Add, Remove } from '@material-ui/icons';
 import styled from 'styled-components'
 import Announcement from '../components/Announcement'
 import Footer from '../components/Footer'
@@ -50,30 +51,93 @@ const Info = styled.div`
     flex: 3;
 `;
 
-const Product = styled.div``;
+const Product = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
 
-const ProductDetail = styled.div``;
+const ProductDetail = styled.div`
+    flex: 2;
+        display: flex;
+`;
 
-const Image = styled.img``;
+const Image = styled.img`
+    width: 200px;
+`;
 
-const Details = styled.div``;
+const Details = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-space-around;
+`;
 
 const ProductName = styled.span``;
 
 const ProductId = styled.span``;
 
-const ProductColor = styled.div``;
+const ProductColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${(props) => props.color};
+`;
 
 const ProductSize = styled.span``;
 
-const PriceDetail = styled.div``;
+const PriceDetail = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
 
+const ProductAmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+`;
 
+const ProductAmount = styled.div`
+    font-size: 24px;
+    margin: 5px;
+`;
 
+const ProductPrice = styled.div`
+    font-size: 30px;
+    font-weight: 200;
+`;
+
+const Hr = styled.hr`
+    background-color: #eee;
+    border: none;
+    height: 1;
+`;
 
 const Summary = styled.div`
     flex: 1;
+    border: 0.5px solid lightgray;
+    border-radius: 10px;
+    padding: 20px;
 `;
+
+const SummaryTitle = styled.h1`
+    font-weight: 200;
+`;
+
+const SummaryItem = styled.h1`
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-space-between;
+    font-weight: ${props => props.type === "total" && "500"};
+    font-size: ${props => props.type === "total" && "24px"};
+`;
+
+const SummaryItemText = styled.span``;
+const SummaryItemPrice = styled.span``;
+
+const Button = styled.button``;
 
 
 
@@ -100,16 +164,60 @@ const Cart = () => {
                                 <Details>
                                     <ProductName><b>Product:</b> JESSIE THUNDER SHOES</ProductName>
                                     <ProductId><b>ID:</b> 93443432423</ProductId>
-                                    <ProductColor />
+                                    <ProductColor color="black"/>
                                     <ProductSize><b>Size:</b> 37.5</ProductSize>
                                 </Details>
                             </ProductDetail>
                             <PriceDetail>
-                                Price
+                                <ProductAmountContainer>
+                                    <Add />
+                                    <ProductAmount>2</ProductAmount>
+                                    <Remove />
+                                </ProductAmountContainer>
+                                <ProductPrice>$30</ProductPrice>
+                            </PriceDetail>
+                        </Product>
+                        <Hr />
+                        <Product>
+                            <ProductDetail>
+                                <Image src=" "/>
+                                <Details>
+                                    <ProductName><b>Product:</b> Cool T-Shirt</ProductName>
+                                    <ProductId><b>ID:</b> 4533934423</ProductId>
+                                    <ProductColor color="gray"/>
+                                    <ProductSize><b>Size:</b> M</ProductSize>
+                                </Details>
+                            </ProductDetail>
+                            <PriceDetail>
+                                <ProductAmountContainer>
+                                    <Add />
+                                    <ProductAmount>2</ProductAmount>
+                                    <Remove />
+                                </ProductAmountContainer>
+                                <ProductPrice>$30</ProductPrice>
                             </PriceDetail>
                         </Product>
                     </Info>
-                    <Summary>summary</Summary>
+                    <Summary>
+                        <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+                        <SummaryItem>
+                            <SummaryItemText>Subtotal</SummaryItemText>
+                            <SummaryItemPrice>$ 80</SummaryItemPrice>
+                        </SummaryItem>
+                        <SummaryItem>
+                            <SummaryItemText>Estimated Shipping</SummaryItemText>
+                            <SummaryItemPrice>$ 5.95</SummaryItemPrice>
+                        </SummaryItem>
+                        <SummaryItem>
+                            <SummaryItemText>Shipping Discount</SummaryItemText>
+                            <SummaryItemPrice>$ -5.90</SummaryItemPrice>
+                        </SummaryItem>
+                        <SummaryItem type="total">
+                            <SummaryItemText>Total</SummaryItemText>
+                            <SummaryItemPrice>$ 80</SummaryItemPrice>
+                        </SummaryItem>
+                        <Button>CHECKOUT NOW</Button>
+                    </Summary>
                 </Bottom>
             </Wrapper>
             <Footer />
